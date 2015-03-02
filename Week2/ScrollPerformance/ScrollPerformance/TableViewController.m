5c8066a77b34456803c8c65df9519b07d1cedcb2
Zachary Davison <zac.developer@gmail.com>
2015-03-02 19:46:08 +0000

    Added Week 2 solution and Week 3

--- Week2/ScrollPerformance/ScrollPerformance/TableViewController.m
+++ Week2/ScrollPerformance/ScrollPerformance/TableViewController.m
@@ -8,7 +8,21 @@
 
 #import "TableViewController.h"
 
+@interface TableViewController()
+
+@property (nonatomic, strong) NSMutableDictionary *cache;
+
+@end
+
 @implementation TableViewController
+
++ (NSMutableDictionary*)sharedCache {
+  static NSMutableDictionary *sharedInstance = nil;
+  static dispatch_once_t  onceToken;
+  dispatch_once(&onceToken,
+                ^{ sharedInstance = [NSMutableDictionary dictionary]; });
+  return sharedInstance;
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
   return 500;
@@ -21,11 +35,34 @@
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
   
   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ImageCell"];
-  NSString *path = [[NSBundle mainBundle] pathForResource:@"hemsworth" ofType:@"jpg"];
-  cell.imageView.image = [UIImage imageWithContentsOfFile:path];
+  
+  NSMutableDictionary *cache = [TableViewController sharedCache];
+  
+  NSString *selectedImage = nil;
+  NSArray *imageNames = @[@"hemsworth", @"placeholder_hemsworth"];
+  if(indexPath.row % 2 == 0){
+    selectedImage = imageNames.firstObject;
+  }else{
+    selectedImage = imageNames.lastObject;
+  }
+  
+  NSString *path = [[NSBundle mainBundle] pathForResource:selectedImage ofType:@"jpg"];
+  
+  UIImage *existingImage = [cache objectForKey:path];
+  NSLog(@"EXISTING IMAGE CALLED: %@",existingImage);
+  if(existingImage == nil){
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+      UIImage *image = [UIImage imageWithContentsOfFile:path];
+      [cache setObject:image forKey:path];
+      dispatch_async(dispatch_get_main_queue(), ^{
+        cell.imageView.image = image;
+      });
+    });
+  }else{
+    cell.imageView.image = existingImage;
+  }
   
   return cell;
-  
 }
 
 @end
